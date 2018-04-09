bf9e56e22e6e179e46daabdbb22522a11fabda23
David Kahn <dkahn200@gmail.com>
2018-04-09 07:13:31 -0400

    MatchMaker
    Work in Progress

---
+++ class_work/class (4)/first_javascript/script.js
@@ -0,0 +1,27 @@
+var thisIsSuchACoolVariableNameThatIJustCalled = "hi ";
+document.write(thisIsSuchACoolVariableNameThatIJustCalled);
+
+
+
+
+
+var greeting = "hey"
+newGreeting = greeting + ", my friends";
+document.write(newGreeting);
+
+
+
+
+
+
+var number = 10 * 10;
+var newNumber = number + 20;
+document.write(newNumber);
+
+
+
+
+function hello() {
+	alert("hello")
+}
+hello()
