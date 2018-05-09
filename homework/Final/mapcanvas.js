dc5f93033291d0d9c602228c3a2f3b4d512fef0c
David Kahn <dkahn200@gmail.com>
2018-05-09 04:41:45 -0400

    update drinking fountain json
    found it!

--- homework/Final/mapcanvas.js
+++ homework/Final/mapcanvas.js
@@ -1,216 +1,216 @@
 
-function initMap() {
+// function initMap() {
 
 
 
-        var mapStyle = [ 
-              {
-                featureType: "all",
-                stylers: [{color: "hsla(107, 86%, 52%, 0.29)"}]
-              },
+//         var mapStyle = [ 
+//               {
+//                 featureType: "all",
+//                 stylers: [{color: "hsla(107, 86%, 52%, 0.29)"}]
+//               },
          
-              {
-                featureType: 'poi.park',
-                elementType: 'geometry.fill',
-                stylers: [{color: '#2abe09'}]
-              },
-              {
-                featureType: 'poi.park',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#447530'}]
-              },
-              {
-                featureType: 'road',
-                elementType: 'geometry',
-                stylers: [{color: '#FFFFFF'}]
-              },
+//               {
+//                 featureType: 'poi.park',
+//                 elementType: 'geometry.fill',
+//                 stylers: [{color: '#2abe09'}]
+//               },
+//               {
+//                 featureType: 'poi.park',
+//                 elementType: 'labels.text.fill',
+//                 stylers: [{color: '#447530'}]
+//               },
+//               {
+//                 featureType: 'road',
+//                 elementType: 'geometry',
+//                 stylers: [{color: '#FFFFFF'}]
+//               },
               
-              {
-                featureType: 'water',
-                elementType: 'geometry.fill',
-                stylers: [{color: '#0377a5'}]
-              },
-              {
-                featureType: "poi",
-                elementType: "labels",
-                stylers: [
-              { visibility: "off" }]
-              },
-              {
-                featureType: 'water',
-                elementType: 'labels.text.fill',
-                stylers: [{color: '#0377a5'}]
-              }
-            ];
+//               {
+//                 featureType: 'water',
+//                 elementType: 'geometry.fill',
+//                 stylers: [{color: '#0377a5'}]
+//               },
+//               {
+//                 featureType: "poi",
+//                 elementType: "labels",
+//                 stylers: [
+//               { visibility: "off" }]
+//               },
+//               {
+//                 featureType: 'water',
+//                 elementType: 'labels.text.fill',
+//                 stylers: [{color: '#0377a5'}]
+//               }
+//             ];
             
 
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.7654898, lng: -73.9373333},
-          zoom: 13,
-          styles: mapStyle,
-          fullscreenControl: false
-        });
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: 40.7654898, lng: -73.9373333},
+//           zoom: 13,
+//           styles: mapStyle,
+//           fullscreenControl: false
+//         });
 
-    $("#waterFountains").click(function(){
-      new google.maps.Marker({
-        position: {lat: 40.798000, lng: -73.952479},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.796809, lng: -73.950002},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.795777, lng: -73.950292},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.794512, lng: -73.951359},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.794231, lng: -73.952575},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.793577, lng: -73.953051},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.792869, lng: -73.954309},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.793034, lng: -73.955495},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.792007, lng: -73.955856},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.791532, lng: -73.955416},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.790564, lng: -73.954686},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.790086, lng: -73.954456},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.790104, lng: -73.954451},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.79023792172633, lng: -73.95766062088958},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.790803, lng: -73.959531},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.79080348614056, lng: -73.95953094076155},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.792024, lng: -73.961296},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.792095, lng: -73.959755},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.793547, lng: -73.960607},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.791643, lng: -73.958266},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.792319, lng: -73.963266},,
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.792319, lng: -73.963266},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.793806, lng: -73.962325},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.79380559740607, lng: -73.96232462490583},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.794008, lng: -73.961903},
-        map: map
-      });
-      new google.maps.Marker({
-        position: {lat: 40.795995, lng: -73.961432},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.798767, lng: -73.958700},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.797269, lng: -73.959005},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.799871, lng: -73.957483},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.797018, lng: -73.951250},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.792974, lng: -73.957292},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.791659, lng: -73.964283},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.791123, lng: -73.964261},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.789471, lng: -73.962030},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.789444, lng: -73.965831},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.789396, lng: -73.963956},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.788807, lng: -73.961937},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.788402, lng: -73.966361},
-        map: map
-      });
-       new google.maps.Marker({
-        position: {lat: 40.787874, lng: -73.956301},
-        map: map
-      });
+//     $("#waterFountains").click(function(){
+//       new google.maps.Marker({
+//         position: {lat: 40.798000, lng: -73.952479},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.796809, lng: -73.950002},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.795777, lng: -73.950292},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.794512, lng: -73.951359},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.794231, lng: -73.952575},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.793577, lng: -73.953051},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.792869, lng: -73.954309},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.793034, lng: -73.955495},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.792007, lng: -73.955856},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.791532, lng: -73.955416},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.790564, lng: -73.954686},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.790086, lng: -73.954456},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.790104, lng: -73.954451},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.79023792172633, lng: -73.95766062088958},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.790803, lng: -73.959531},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.79080348614056, lng: -73.95953094076155},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.792024, lng: -73.961296},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.792095, lng: -73.959755},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.793547, lng: -73.960607},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.791643, lng: -73.958266},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.792319, lng: -73.963266},,
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.792319, lng: -73.963266},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.793806, lng: -73.962325},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.79380559740607, lng: -73.96232462490583},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.794008, lng: -73.961903},
+//         map: map
+//       });
+//       new google.maps.Marker({
+//         position: {lat: 40.795995, lng: -73.961432},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.798767, lng: -73.958700},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.797269, lng: -73.959005},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.799871, lng: -73.957483},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.797018, lng: -73.951250},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.792974, lng: -73.957292},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.791659, lng: -73.964283},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.791123, lng: -73.964261},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.789471, lng: -73.962030},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.789444, lng: -73.965831},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.789396, lng: -73.963956},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.788807, lng: -73.961937},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.788402, lng: -73.966361},
+//         map: map
+//       });
+//        new google.maps.Marker({
+//         position: {lat: 40.787874, lng: -73.956301},
+//         map: map
+//       });
 
-    })
+//     })
 
-}
+// }
 
