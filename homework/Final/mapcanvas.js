5d6f711ed23649934c6083f2a0a05bd92d1f379e
David Kahn <dkahn200@gmail.com>
2018-05-13 23:14:48 -0400

    updated final

--- homework/Final/mapcanvas.js
+++ homework/Final/mapcanvas.js
@@ -1,4 +1,7 @@
 
+var markers = [];
+      // This global polygon variable is to ensure only ONE polygon is rendered.
+      var polygon = null;
 var map, infoWindow;
 function initMap() {
 
@@ -50,13 +53,24 @@ function initMap() {
           styles: mapStyle,
           fullscreenControl: false,
           mapTypeControl: true,
-    	  mapTypeControlOptions: {
+    	    mapTypeControlOptions: {
         	style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
         	position: google.maps.ControlPosition.LEFT_BOTTOM
     		}
         });
+      var markers = []
+     $("#waterFountains").click(setMarkers)
+      function setMarkers() {  
+      if (markers.length > 0) {
+        //remove markers
+        markers.forEach(removeMarker)
+       function removeMarker(marker) {
+          marker.setMap(null);
+       }
+       markers = []
+      }
+      else {
 
-         
       $.ajax({
       type: 'GET',
       dataType:'json',
@@ -66,81 +80,50 @@ function initMap() {
       waterFountains.elements.forEach(dropMarker)
 
       function dropMarker(waterFountain) {
-        new google.maps.Marker({
+        var marker = new google.maps.Marker({
           position: {lat: waterFountain.lat, lng: waterFountain.lon},
-          map: map
-        });
+          map: map,
+          icon: defaultIcon
+              });
+        markers.push(marker)
+            }
+          },
+        })
       }
-    },
-  })
 
-
-
-      // Data for the markers consisting of a name, a LatLng and a zIndex for the
-      // order in which these markers should display on top of each other.
-      
-      // var waterfountains = [
-      //   [40.798000, -73.952479],
-      //   [40.796809, -73.950002],
-      //   [40.794512, -73.951359],
-      //   [40.794231, -73.952575],
-      //   [40.793577, -73.953051],
-      //   [40.794231, -73.952575],
-      //   [40.793577, -73.953051],
-      //   [40.792869, -73.954309],
-      //   [40.793034, -73.955495],
-      //   [40.792007, -73.955856],
-      //   [40.791532, -73.955416],
-      //   [40.790564, -73.954686],
-      //   [40.790086, -73.954456],
-      //   [40.790104, -73.954451],
-      //   [40.79023792172633, -73.95766062088958],
-      //   [40.790803, -73.959531],
-      //   [40.79080348614056, -73.95953094076155],
-      //   [40.792024, -73.961296],
-      //   [40.792095, -73.959755],
-      //   [40.793547, -73.960607],
-      //   [40.791643, -73.958266],
-      //   [40.792319, -73.963266]
-
-      // ];
-$("#waterFountains").click(setMarkers)
-      function setMarkers() {
-        // Adds markers to the map.
-
-        // Marker sizes are expressed as a Size of X,Y where the origin of the image
-        // (0,0) is located in the top left of the image.
-
-        // Origins, anchor positions and coordinates of the marker increase in the X
-        // direction to the right and in the Y direction down.
-        var image = {
-          url: 'img/drinking-water.png',
-          // This marker is 20 pixels wide by 32 pixels high.
-          size: new google.maps.Size(20, 24),
-          // The origin for this image is (0, 0).
-          origin: new google.maps.Point(0, 0),
-          // The anchor for this image is the base of the flagpole at (0, 32).
-          anchor: new google.maps.Point(0, 24)
-        };
-        // Shapes define the clickable region of the icon. The type defines an HTML
-        // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-        // The final coordinate closes the poly by connecting to the first coordinate.
-        var shape = {
-          coords: [1, 1, 1, 20, 18, 20, 18, 1],
-          type: 'poly'
-        };
-        for (var i = 0; i < waterfountains.length; i++) {
-          var waterfountain = waterfountains[i];
+var defaultIcon = makeMarkerIcon('0091ff');
+        // Create a "highlighted location" marker color for when the user
+        // mouses over the marker.
+        var highlightedIcon = makeMarkerIcon('FFFF24');
+        // The following group uses the location array to create an array of markers on initialize.
+        for (var i = 0; i < locations.length; i++) {
+          // Get the position from the location array.
+          var position = locations[i].location;
+          var title = locations[i].title;
+          // Create a marker per location, and put into markers array.
           var marker = new google.maps.Marker({
-            position: {lat: waterfountain[0], lng: waterfountain[1]},
-            map: map,
-            icon: image,
-            shape: shape,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            icon: defaultIcon,
+            id: i
+          });
+          // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open the large infowindow at each marker.
+          marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+          });
+          // Two event listeners - one for mouseover, one for mouseout,
+          // to change the colors back and forth.
+          marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+          });
+          marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
           });
         }
-      }
-
-
+}
 
         infoWindow = new google.maps.InfoWindow;
 
@@ -151,7 +134,7 @@ $("#waterFountains").click(setMarkers)
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-
+console.log(pos)
             infoWindow.setPosition(pos);
             infoWindow.setContent('You are here!');
             infoWindow.open(map);
@@ -172,16 +155,10 @@ $("#waterFountains").click(setMarkers)
                               'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
 
-Notification.requestPermission(function(result) {  
-  if (result === 'denied') {  
-    console.log('Permission wasn\'t granted. Allow a retry.');  
-    return;  
-  } else if (result === 'default') {  
-    console.log('The permission request was dismissed.');  
-    return;  
-  }  
-  console.log('Permission was granted for notifications');  
-});
+
+
+
+
 
 }
   
