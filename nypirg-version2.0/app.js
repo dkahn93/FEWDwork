7b5a02d5da0f9e12ccaa28bd7ba8d1529498aeed
David Kahn <dkahn200@gmail.com>
2018-08-20 17:44:18 -0400

    lots of things

---
+++ nypirg-version2.0/app.js
@@ -0,0 +1,56 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+$(".content").click(function(event) {
+  $(event.currentTarget).find(".wrapper").slideToggle();
+});
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 300) {
+        $('#navbar').addClass('opaque');
+    } else {
+        $('#navbar').removeClass('opaque');
+    }
+});
+
+// var $animation_elements = $('.animation-element');
+// var $window = $(window);
+
+// function check_if_in_view() {
+//   var window_height = $window.height();
+//   var window_top_position = $window.scrollTop();
+//   var window_bottom_position = (window_top_position + window_height);
+
+//   $.each($animation_elements, function() {
+//     var $element = $(this);
+//     var element_height = $element.outerHeight();
+//     var element_top_position = $element.offset().top;
+//     var element_bottom_position = (element_top_position + element_height);
+
+//     //check to see if this current container is within viewport
+//     if ((element_bottom_position >= window_top_position) &&
+//       (element_top_position <= window_bottom_position)) {
+//       $element.addClass('in-view');
+//     } else {
+//       $element.removeClass('in-view');
+//     }
+//   });
+// }
+
+// $window.on('scroll resize', check_if_in_view);
+// $window.trigger('scroll');
